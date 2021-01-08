beb1785a851318402ab4a0280da29a492001dc29
ChillyWilly <noreply>
2021-01-08 15:02:28 -0500

    fightOrSkip() to handle mixed case and blank input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,25 @@
-var fight = function (enemy) {
-    while (enemy.health > 0 && playerInfo.health > 0) {
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
-        if (promptFight === "skip" || promptFight === "SKIP") {
+var fightOrSkip = function() {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+    promptFight = promptFight.toLowerCase();
+    if (!promptFight) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+        if (promptFight === "skip") {
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
             if (confirmSkip) {
                 window.alert(playerInfo.name + " has decided to skip the fight. Goodbye!");
                 playerInfo.money = Math.max(0, playerInfo.money - 10);
-                break;
+                return true;
             }
+        }
+        return false;
+}
+
+var fight = function (enemy) {
+    while (enemy.health > 0 && playerInfo.health > 0) {
+        if (fightOrSkip()) {
+            break;
         }
 
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
